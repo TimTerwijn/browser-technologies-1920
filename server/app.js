c534d37438a84a147568c8a33e40f3a9b81cbc26
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2020-03-23 08:50:51 +0100

    Can add images now

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const multer  = require('multer');
+
 const port = 3000;
 
 // Tell the views engine/ejs where the template files are stored (Settingname, value)
@@ -13,6 +15,18 @@ app.set('view engine', 'ejs');
 
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, "../client/static/img/"); //here we specify the destination. in this case i specified the current directory
+  },
+  filename: function(req, file, cb) {
+    console.log(file); //log the file object info in console
+    cb(null, file.originalname);//here we specify the file saving name. in this case. i specified the original file name .you can modify this name to anything you want
+  }
+});
+
+const uploadDisk = multer({ storage: storage });
 
 
 // ROUTES!!!
@@ -34,9 +48,15 @@ app.get('/image/:id', function(req, res) {
 });
 
 // POST upload new image 
-app.post('/insert-image', function(req, res) {
+app.get('/insert-image', function(req, res) {
    
-  res.render('index', null);
+  res.render('add-image', null);
+});
+
+// POST upload new image 
+app.post('/insert-image', uploadDisk.single("picture"), (req, res) => {
+  console.log(" file disk uploaded");
+  res.send("file disk upload success");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
