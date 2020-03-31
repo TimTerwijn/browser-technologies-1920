55e44824f368a022ff6e413151f05721b839ab6e
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2020-03-31 15:06:42 +0200

    Courusel works!

--- client/static/js/app.js
+++ client/static/js/app.js
@@ -1,10 +1,92 @@
-
 function init(){
+    const newImage = document.querySelector("main>section:nth-child(3)>a>img:first-child");
+    newImage.id = "selectedImage"; 
+    
     //fix all the buttons
-    buttonSetup();
+    enableButtons();
 };
 
-function buttonSetup(){
+function nextImage(){
+    blockButtons();
+    
+    //find curent image
+    const currentImage = document.getElementById("selectedImage");
+    
+    //find the next image
+    let nextImage = currentImage.nextElementSibling;
+
+    //check next image
+    if(nextImage !== null){
+        currentImage.id = null; 
+        nextImage.id = "selectedImage";
+    }else{
+        //put nextImage below curent image
+        nextImage = document.querySelector("main>section:nth-child(3)>a>img:first-child");
+        currentImage.insertAdjacentElement("afterend", nextImage);
+
+        //wait for that the image to be refactored
+        setTimeout(function (){
+            //do animation
+            currentImage.id = null; 
+            nextImage.id = "selectedImage"; 
+        }, 20);
+    }
+
+    //wait for animation end
+    setTimeout(function (){
+        enableButtons();
+    }, 1500);    
+}
+
+function previousImage(){
+    blockButtons();
+    
+    //find curent image
+    const currentImage = document.getElementById("selectedImage");
+    
+    //find the previous image
+    let previousImage = currentImage.previousElementSibling;
+
+    //check previous image
+    if(previousImage === null){
+        previousImage = document.querySelector("main>section:nth-child(3)>a>img:last-child");
+    }
+
+    //move previous image under current image for a effect
+    currentImage.insertAdjacentElement("afterend", previousImage);
+    blockButtons();
+
+    //wait for that the image to be refactored
+    setTimeout(function (){
+        //do animation
+        currentImage.id = null; 
+        previousImage.id = "selectedImage"; 
+
+        //wait for animation end
+        setTimeout(function (){
+            //reverse move (move current image under next image)
+            previousImage.insertAdjacentElement("afterend", currentImage);
+            enableButtons();
+        }, 1500);
+    }, 100);
+}
+
+function blockButtons(){
+    //get buttons
+    const nextButton = document.querySelector("main>section:nth-child(2)>a:nth-child(2)");
+    const previousButton = document.querySelector("main>section:nth-child(2)>a:nth-child(1)");
+    
+    // remove anchor functions & set onclicks
+    nextButton.onclick = function(){
+        event.preventDefault();
+    }
+
+    previousButton.onclick = function(){
+        event.preventDefault();
+    } 
+}
+
+function enableButtons(){
     //get buttons
     const nextButton = document.querySelector("main>section:nth-child(2)>a:nth-child(2)");
     const previousButton = document.querySelector("main>section:nth-child(2)>a:nth-child(1)");
@@ -19,20 +101,6 @@ function buttonSetup(){
         event.preventDefault();
         previousImage();
     } 
-}
-
-function nextImage(){
-    //move first image to the back
-    const parent = document.querySelector("main>section:nth-child(3)>a");
-    const child = parent.firstElementChild;
-    parent.append(child);
-}
-
-function previousImage(){
-     //move last image to the front
-    const parent = document.querySelector("main>section:nth-child(3)>a");
-    const child = parent.lastElementChild;
-    parent.prepend(child); 
 }
 
 init();
